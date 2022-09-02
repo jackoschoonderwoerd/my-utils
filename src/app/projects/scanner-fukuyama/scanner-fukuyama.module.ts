a6fb35707bfaafa20dbd7a7d1b94f7cc63e5b84f
jackoschoonderwoerd <34942572+jackoschoonderwoerd@users.noreply.github.com>
2022-09-02 16:35:56 +0200

    scanners

---
+++ src/app/projects/scanner-fukuyama/scanner-fukuyama.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ScannerFukuyamaRoutingModule } from './scanner-fukuyama-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ScannerFukuyamaRoutingModule
+  ]
+})
+export class ScannerFukuyamaModule { }
